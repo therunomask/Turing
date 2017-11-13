102965d9163f01f32b115b8fd1d38d20574d1d27
Mschkin <christianbild1@web.de>
2017-11-13 22:11:12 +0100

    jetyt gehts

--- src/main.rs
+++ src/main.rs
@@ -4,39 +4,20 @@
 //fn matrix_vector_mult()
 
 fn main() {
-    //vecto(1200);
     let mat = Mat6 {
-        dat: [
-            [0., 1., 1., 1., 0., 2.],
-            [0., 0., 1., 0., 1., 1.],
-            [2., 2., 1., 1., -2., -1.],
-            [-2., -1., -1., 0., 1., 2.],
-            [0., 0., 0., 0., 0., 0.],
-            [0., 0., 0., 0., 0., 0.],
-        ],
+        dat: [[0., 1., 1., 1., 0., 0.], [0., 0., 1., 0., 1., -1.], [2., 2., 1.,
+  1., -2., -1.], [-2., -1., -1., 0., 1., 2.], [0., 0., 0., 0., 0.,
+  0.], [0., 0., 0., 0., 0., 0.]],
     };
-    //mat.print();
-    let inpu: Vec<bool> = vec![true, false, true, false];
-    println!("{:?}", Mat6::matpro(&mat, vbool_to_vf32(&logic(inpu))));
-    let mut testband = Band::new(4);
-    println!("{}", testband.get_value());
-    //testband.mover(true);
-    testband.overwrite(true);
+    let mut testband = Band::new(300);
+    print!("The Band is: ");
     testband.print_band();
     let mut state_vector: Vec<bool> = vec![true, true, false, false, true, true];
     let mut terminated = false;
-    let mut a = false;
-    while a == false {
-        print!("a");
-        a = true
-    }
     while terminated == false {
-        println!("{}", terminated);
-        println!("this happens");
         let (terminatedbla, state_vectorbla) = organiser(&state_vector, &mat, &mut testband);
         state_vector = state_vectorbla;
         terminated = terminatedbla;
-        println!("{:?}", state_vector);
     }
 }
 
@@ -74,10 +55,6 @@ fn vecto(mut zahl: i64) -> Vec<bool> {
         zahl = zahl / 2;
     }
     ve.reverse();
-    for i in 0..ve.len() {
-        println!("{:?}", ve[i])
-    }
-    println!("{:?}", ve);
     ve
 }
 
@@ -151,22 +128,30 @@ fn organiser(
     mat: &Mat6,
     mut testband: &mut Band,
 ) -> (bool, Vec<bool>) {
+    //0 bandwert 1,2,3 zustand der turing machine 5,6 logic darauf angewendet
     let mut state_vector_float = vbool_to_vf32(&state_vector);
     let mut new_state = Mat6::matpro(&mat, state_vector_float);
     let mut direction: bool;
     let mut new_state_bool: Vec<bool>;
     let (direction, new_state_bool) = after_matrix_cast(new_state);
     let mut terminated = testband.mover(direction);
-    if terminated == false {
+    let mut new_state_return: Vec<bool>;
+    if terminated == true {
+        print!("The final states are: ");
         println!("{:?}", new_state_bool);
+        new_state_return= new_state_bool;
     }
-    let vec = vec![
+    else{
+        let vec = vec![
         testband.get_value(),
         new_state_bool[0],
         new_state_bool[1],
         new_state_bool[2],
     ];
-    (terminated, logic(vec))
+    new_state_return=logic(vec);
+    }
+  
+    (terminated, new_state_return)
 }
 
 struct Band {
@@ -179,13 +164,12 @@ impl Band {
         self.band[self.position]
     }
     pub fn mover(&mut self, direction: bool) -> bool {
-        println!("{}", self.position);
         if direction {
             self.position += 1;
         } else {
             self.position -= 1;
         }
-        if self.position < self.band.len() - 1 {
+        if self.position < self.band.len() {
             false
         } else {
             true
